39e5aaf1b933dfe869ee7adc8986803a9881fe5a
Nicolas Primeau <nicolas.primeau@gmail.com>
2016-04-02 14:47:43 -0400

    Maiden voyage of The Democratic Republic of Mouth-LDAs

--- system/analysis/GaborAnalyser.py
+++ system/analysis/GaborAnalyser.py
@@ -4,11 +4,18 @@ from scipy import misc
 import numpy as np
 import glob
 import datetime
-from multiprocessing import Process, Manager
+import ctypes as c
+from multiprocessing import Process, Array
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import warnings
 warnings.filterwarnings('ignore')
 
+BASE_PATH = "/home/nixon/PycharmProjects/SYSC5405Project"
+JETS = 88
+VECTORS = 72
+MAX_IMAGES_PER_CLASS = None
+CROSS_VALIDATION = 5
+DEBUG = False
 
 CLASSES = {
     "Up": 0,
@@ -19,87 +26,154 @@ CLASSES = {
     "Open": 5
 }
 
+CLASS_ID_LOOKUP = ["" for key in CLASSES]
+for key in CLASSES:
+    CLASS_ID_LOOKUP[CLASSES[key]] = key
+
 
 def main():
-    manager = Manager()
+    train_data = dict()
+    if CROSS_VALIDATION is None:
+        for key in CLASSES:
+            train_data[key] = glob.glob(BASE_PATH + "/data/train/"+key+"/*")
+            if MAX_IMAGES_PER_CLASS is not None and MAX_IMAGES_PER_CLASS < len(train_data[key]):
+                train_data[key] = train_data[key][:MAX_IMAGES_PER_CLASS]
+        test_data = glob.glob(BASE_PATH + "/data/test/*")
+        gabor_analyse(train_data, test_data)
+    else:
+        total_data = dict()
+        for key in CLASSES:
+            total_data[key] = glob.glob(BASE_PATH + "/data/train/"+key+"/*")
+            if MAX_IMAGES_PER_CLASS is not None and MAX_IMAGES_PER_CLASS < len(total_data[key]):
+                total_data[key] = total_data[key][:MAX_IMAGES_PER_CLASS]
+            total_data[key] = slice_list(total_data[key], CROSS_VALIDATION)
 
-    real_jets = manager.dict()
-    imag_jets = manager.dict()
+        for i in range(CROSS_VALIDATION):
+            print('-' * 60)
+            print("Cross Validation fold " + str(i))
+            print('-' * 60 + '\n')
+            train_data = dict()
+            test_data_classes = list()
+            test_data = list()
+            for key in CLASSES:
+                train_data[key] = list()
+                for slice in range(len(total_data[key])):
+                    if slice != i:
+                        train_data[key].extend(total_data[key][slice])
+                    else:
+                        test_data.extend(total_data[key][slice])
+                        test_data_classes.extend([CLASSES[key]]*len(total_data[key][slice]))
+            predictions = gabor_analyse(train_data, test_data)
+            precise, wrong, tie = precision_assesment(predictions, test_data_classes)
+            print('-' * 60)
+            print("Cross validation run " + str(i))
+            print("Correct: " + str(precise))
+            print("Wrong: " + str(wrong))
+            print("Tie: " + str(tie))
+            print("Total: " + str(len(test_data)))
+            print('-' * 60)
 
-    folders = glob.glob("../../data/train/*/")
 
-    names = [target.split("/")[-2] for target in folders]
-    for name in names:
-        real_jets[name] = np.array([])
-        imag_jets[name] = np.array([])
+def precision_assesment(predictions, test_data_classes):
+    tie = 0
+    precise = 0
+    wrong = 0
+    for j in range(len(test_data_classes)):
+        prediction = predictions[j]
+        if len(prediction) > 1:
+            tie += 1
+        else:
+            prediction = prediction[0]
+            actual = test_data_classes[j]
+            if prediction == actual:
+                precise += 1
+            else:
+                wrong += 1
+    return precise, wrong, tie
 
-    processes = [Process(target=analyse, args=(folder, real_jets, imag_jets, )) for folder in folders]
+
+def slice_list(seq, num):
+  avg = len(seq) / float(num)
+  out = []
+  last = 0.0
+
+  while last < len(seq):
+    out.append(seq[int(last):int(last + avg)])
+    last += avg
+
+  return out
+
+
+def gabor_analyse(train_data, test_data):
+
+    features_per_class = dict()
+
+    print("Allocating Memory")
+    for key in CLASSES:
+        number_of_pictures = len(train_data[key])
+        features_per_class[key] = np.frombuffer(Array(c.c_double, JETS*number_of_pictures*(2*VECTORS+1)).get_obj())
+        features_per_class[key] = features_per_class[key].reshape((JETS, number_of_pictures, (2*VECTORS+1)))
+
+    print("Processing images")
+    processes = [Process(target=analyse, args=(CLASSES[key], train_data[key], features_per_class)) for key in CLASSES]
     [p.start() for p in processes]
     [p.join() for p in processes]
 
-    # Put wavelets as main index
-
-    matrix_real = None
-    matrix_imag = None
     print("Preparing Data")
-    for name in names:
-        real_jets[name] = real_jets[name]
-        imag_jets[name] = imag_jets[name]
-        if matrix_real is None:
-            matrix_real = [dict() for i in range(len(real_jets[name]))]
-            matrix_imag = [dict() for i in range(len(real_jets[name]))]
-        # Name - Jet - Vector - Val
-        # Jet1 - Names - Vector - Val
-        for i in range(len(real_jets[name])):
-            matrix_real[i][name] = real_jets[name][i]
-            matrix_imag[i][name] = imag_jets[name][i]
+    # NAME x JETS x SAMPLES x feature vector
+    # 88 x (NAMExSAMPLES) x feature vector
 
-    del real_jets
-    del imag_jets
+    features = [np.row_stack(([features_per_class[key][jet] for key in CLASSES]))
+                for jet in range(len(features_per_class[next(iter(CLASSES.keys()))]))]
 
-    predictions = manager.list()
-    [predictions.append(list()) for i in range(len(matrix_real))]
+    del features_per_class
 
-    test_files = glob.glob("../../data/test/*")
-    print("Starting Classifiers")
-    processes = [Process(target=train_and_predict, args=(i, matrix_real[i], matrix_imag[i], test_files, predictions)) for i in range(len(matrix_real))]
-    [p.start() for p in processes]
-    [p.join() for p in processes]
+    print("Training Classifiers")
+    classifiers = list()
+    for feature_set in range(len(features)):
+        classifiers.append(train(features[feature_set]))
+        now = datetime.datetime.now()
+        if DEBUG:
+            print(str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) +
+                "  -  Done training classifier " + str(feature_set+1) + "/" + str(len(features)))
+    del features
 
-    del matrix_real
-    del matrix_imag
+    print("Testing Classifiers")
 
-    test_file_predictions = [[0 for i in range(6)] for j in range(len(test_files))]
-    for classifier in range(len(predictions)):
-        for image in range(len(predictions[classifier])):
-            prediction = predictions[classifier][image]
-            test_file_predictions[image][prediction] += 1
-
-    for predict in range(len(test_file_predictions)):
-        print(test_files[predict] + " : " + ",".join([str(really) for really in test_file_predictions[predict]]))
+    winners, votes = predict(classifiers, test_data)
+    if DEBUG:
+        for image_id in range(len(test_data)):
+            labeled = [(CLASS_ID_LOOKUP[classe] + '(' + str(votes[image_id][classe]) + ')')
+                       for classe in range(len(votes[image_id]))]
+            print('-' * 40)
+            print("File: " + test_data[image_id])
+            print("Winner: " + ", ".join([CLASS_ID_LOOKUP[win] for win in winners[image_id]]))
+            print("Votes: " + ", ".join(labeled))
 
     print("Analysis done")
+    return winners
 
 
-def train_and_predict(jetId, jet_real, jet_imag, test_data, predictions):
-
-    global CLASSES
+def train(features):
     samples = list()
     classes = list()
-
-    # 6 x N_IMAGES x 72
-    # N_IMAGES x 144
-
-    for key in jet_real:
-        for i in range(len(jet_real[key])):
-            samples.append(jet_real[key][i].tolist() + jet_imag[key][i].tolist())
-            classes.append(CLASSES[key])
+    np.random.shuffle(features)
+    for feature in features:
+        feature = feature.tolist()
+        classe = feature.pop()
+        samples.append(feature)
+        classes.append(classe)
 
     classifier = LinearDiscriminantAnalysis(solver="eigen", shrinkage="auto")
     classifier.fit(samples, classes)
+    return classifier
 
-    to_predict = list()
 
+def predict(classifiers, test_data):
+    predictions_votes = list()
+    winners = list()
+
+    cnter = 0
     for image_name in test_data:
         image = misc.imread(image_name, mode="L")
         gray_scale = misc.imresize(image, (63, 83))
@@ -110,21 +184,31 @@ def train_and_predict(jetId, jet_real, jet_imag, test_data, predictions):
         gwt = bob.ip.gabor.Transform(number_of_scales=9)
         trafo_image = gwt(gray_scale)
 
+        # noinspection PyArgumentList
         jets = extractor.extract(trafo_image)
-        to_predict.append(jets[jetId].complex.real.tolist() + jets[jetId].complex.imag.tolist())
+        votes = [0] * len(CLASSES)
+        for jet in range(len(jets)):
+            classe = classifiers[jet].predict([np.concatenate((jets[jet].complex.real, jets[jet].complex.imag))])[0]
+            votes[int(classe)] += 1
+        predictions_votes.append(votes)
+        m = max(votes)
+        winners.append([i for i, j in enumerate(votes) if j == m])
 
-    predictions[jetId] = classifier.predict(to_predict)
+        if cnter % 250 == 0 and DEBUG:
+            now = datetime.datetime.now()
+            print(str(now.hour)+":"+str(now.minute)+":"+str(now.second)+" - Classifying image "+str(cnter))
+        cnter += 1
+    now = datetime.datetime.now()
+    print(str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + " - Done classifying " + str(cnter)+" images")
+    return winners, predictions_votes
 
 
-def analyse(target, real, imag):
-
+def analyse(classe_number, targets, features):
     cnter = 0
-    sub_real = list()
-    sub_imag = list()
+    classe = CLASS_ID_LOOKUP[classe_number]
+    classe_number = [classe_number]
 
-    target_name = target.split("/")[-2]
-
-    for name in glob.glob(target + "*"):
+    for name in targets:
         image = misc.imread(name, mode="L")
         gray_scale = misc.imresize(image, (63, 83))
 
@@ -134,55 +218,22 @@ def analyse(target, real, imag):
         gwt = bob.ip.gabor.Transform(number_of_scales=9)
         trafo_image = gwt(gray_scale)
 
+        # noinspection PyArgumentList
         jets = extractor.extract(trafo_image)
-        sub_real.append(np.array([x.complex.real for x in jets]))
-        sub_imag.append(np.array([x.complex.imag for x in jets]))
 
-        if cnter % 250 == 0:
+        for jet in range(len(jets)):
+            np.copyto(features[classe][jet][cnter],
+                      np.concatenate((jets[jet].complex.real, jets[jet].complex.imag, classe_number)))
+
+        if cnter % 250 == 0 and DEBUG:
             now = datetime.datetime.now()
-            print(str(now.hour)+":"+str(now.minute)+":"+str(now.second)+"  -  " + target_name + "  -  "+str(cnter))
-
+            print(str(now.hour)+":"+str(now.minute)+":"+str(now.second)+" - Analyzing " + classe + " - "+str(cnter))
         cnter += 1
 
-    print(str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + "  -  " + target_name + "  -  Gathering Statistics")
+    now = datetime.datetime.now()
+    print(str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + " - Analyzing " +
+          classe + " - Done! " + str(cnter) + " images processed")
 
-    real_jets = [None for i in range(len(sub_real[0]))]
-    imag_jets = [None for i in range(len(sub_imag[0]))]
-
-    for i in range(len(sub_real)):
-        for j in range(len(sub_real[i])):
-
-            if real_jets[j] is None:
-                real_jets[j] = list()
-                imag_jets[j] = list()
-
-            real_jets[j].append(sub_real[i][j])
-            imag_jets[j].append(sub_imag[i][j])
-
-    real[target_name] = real_jets
-    imag[target_name] = imag_jets
-
-    print(str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + "  -  " + target_name + "  -  Done!")
-
-
-def print_to_file(prefix, data, names):
-    header = "".join([name+"\tstddev\t" for name in names]) + "\n"
-
-    matrix = None
-
-    with open("../../dump/" + prefix, "w") as f:
-        for name in names:
-            if matrix is None:
-                matrix = [[data[name][i][j].tolist() for j in range(len(data[name][i]))] for i in range(len(data[name]))]
-            else:
-                [[matrix[i][j].extend(data[name][i][j]) for j in range(len(data[name][i]))] for i in range(len(data[name]))]
-
-        for i in range(len(matrix)):
-            f.write("Jet-" + str(i) + "\n")
-            f.write(header)
-            for vector in matrix[i]:
-                f.write("\t".join(str(v) for v in vector) + "\n")
-            f.write("\n\n")
 
 if __name__ == "__main__":
     main()
